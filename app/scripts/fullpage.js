d8101bfa6e51eb04c4917137514fc9cc2efacd86
Claret I <clareti@TAG.DC>
2023-12-06 18:37:53 +0530

    type value added

--- app/scripts/fullpage.js
+++ app/scripts/fullpage.js
@@ -22,14 +22,15 @@ const builtHomeHTMLContent = async () => {
   textElement.innerHTML = addChecklist;
 };
 
-function Tree(name, child) {
+function Tree(name, type, child) {
   this.name = name;
   this.children = child || [];
-  this.addNode = function (parent) {
+  this.type = type;
+  this.addNode = function (parent, type) {
     this.children = parent;
   };
-  this.addChild = function (parentName) {
-    this.children.push(new Tree(parentName));
+  this.addChild = function (parentName, type) {
+    this.children.push(new Tree(parentName, type));
   };
 }
 
@@ -48,40 +49,46 @@ const HTMLContentChild = async (randomdigit) => {
   var checklist_name = `<br/><div><input class=child_box id=child_box_${randomdigit} type="text" placeholder="Checklist Name" />
                             <button type="button" onClick=HTMLContentChild() id=add_child>Add Child</button></div>`;
   textElement.innerHTML += checklist_name;
-  var tree = new Tree("Customer Onboarding"); // create a tree (or a portion of a tree) with root "A" and empty children
-  tree.addChild("Aadhar"); // A -> B1
+  var tree = new Tree("Customer Onboarding", "root"); // create a tree (or a portion of a tree) with root "A" and empty children
+  tree.addChild("Aadhar", "text"); // A -> B1
 
-  tree.children[0].addChild("Yes"); // add this tree under Aadher->Yes
-  tree.children[0].addChild("No"); // add this tree under Aadhar->No
+  tree.children[0].addChild("Yes", "button"); // add this tree under Aadher->Yes
+  tree.children[0].addChild("No", "button"); // add this tree under Aadhar->No
 
-  tree.children[0].children[0].addChild("Does Customer have PAN ?"); // add this tree under Aadhar->Yes->PAN?
-  tree.children[0].children[0].children[0].addChild("Yes"); // add this tree under Aadhar->Yes->PAN?->Yes
+  tree.children[0].children[0].addChild("Does Customer have PAN ?", "text"); // add this tree under Aadhar->Yes->PAN?
+  tree.children[0].children[0].children[0].addChild("Yes", "button"); // add this tree under Aadhar->Yes->PAN?->Yes
   tree.children[0].children[0].children[0].children[0].addChild(
-    "Has customer filled the Application ?"
+    "Has customer filled the Application ?",
+    "text"
   ); // add this tree under Aadhar->Yes->PAN?->Yes->filled application
 
   tree.children[0].children[0].children[0].children[0].children[0].addChild(
-    "Yes"
+    "Yes",
+    "button"
   ); // add this tree under Aadhar->Yes->PAN?->Yes->filled application->yes
 
   tree.children[0].children[0].children[0].children[0].children[0].children[0].addChild(
-    "Done"
+    "Done",
+    "text"
   ); // add this tree under Aadhar->Yes->PAN?->Yes->filled application->yes->done
 
   tree.children[0].children[0].children[0].children[0].children[0].addChild(
-    "No"
+    "No",
+    "button"
   ); // add this tree under Aadhar->Yes->PAN?->Yes->filled application->no
 
   tree.children[0].children[0].children[0].children[0].children[0].children[1].addChild(
-    "Request the Customer to do it"
+    "Request the Customer to do it",
+    "text"
   ); // add this tree under Aadhar->Yes->PAN?->Yes->filled application->no->fill the form
 
-  tree.children[0].children[0].children[0].addChild("No"); // add this tree under Aadhar->Yes->PAN?->No
+  tree.children[0].children[0].children[0].addChild("No", "button"); // add this tree under Aadhar->Yes->PAN?->No
   tree.children[0].children[0].children[0].children[1].addChild(
-    "Ask customer to provide PAN"
+    "Ask customer to provide PAN",
+    "text"
   ); // add this tree under Aadhar->Yes->PAN?->No->request PAN
 
-  tree.children[0].children[1].addChild("Request Aadhar"); // add this tree under Aadhar->No->Request Aadhar
+  tree.children[0].children[1].addChild("Request Aadhar", "text"); // add this tree under Aadhar->No->Request Aadhar
 
   console.log(tree);
 };
