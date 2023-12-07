b7259b2f6b6fdab48fff5bf24e4f579277300d07
Claret I <clareti@TAG.DC>
2023-12-07 11:45:13 +0530

    list of guide sshown in modal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,96 @@ init();
 
 async function init() {
   client = await app.initialized();
-  client.events.on("app.activated", renderText);
+  client.events.on("app.activated");
 }
 
-async function renderText() {}
+// async function renderText() {}
+
+const sapmle_tree = [
+  {
+    name: "Customer Onboarding",
+    children: [
+      {
+        name: "Aadhar",
+        children: [
+          {
+            name: "Yes",
+            children: [
+              {
+                name: "Does Customer have PAN ?",
+                children: [
+                  {
+                    name: "Yes",
+                    children: [
+                      {
+                        name: "Has customer filled the Application ?",
+                        children: [
+                          {
+                            name: "Yes",
+                            children: [
+                              {
+                                name: "Done",
+                                children: [],
+                                type: "text",
+                              },
+                            ],
+                            type: "button",
+                          },
+                          {
+                            name: "No",
+                            children: [
+                              {
+                                name: "Request the Customer to do it",
+                                children: [],
+                                type: "text",
+                              },
+                            ],
+                            type: "button",
+                          },
+                        ],
+                        type: "text",
+                      },
+                    ],
+                    type: "button",
+                  },
+                  {
+                    name: "No",
+                    children: [
+                      {
+                        name: "Ask customer to provide PAN",
+                        children: [],
+                        type: "text",
+                      },
+                    ],
+                    type: "button",
+                  },
+                ],
+                type: "text",
+              },
+            ],
+            type: "button",
+          },
+          {
+            name: "No",
+            children: [
+              {
+                name: "Request Aadhar",
+                children: [],
+                type: "text",
+              },
+            ],
+            type: "button",
+          },
+        ],
+        type: "text",
+      },
+    ],
+    type: "root",
+  },
+];
 
 $("#show_checklist").click(async function () {
+  console.log(sapmle_tree);
   console.log("Clicked");
   try {
     let data = await client.interface.trigger("showModal", {
--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -1,8 +1,106 @@
+// var client;
+
 init();
 
 async function init() {
   client = await app.initialized();
-  client.events.on("app.activated", renderText);
+  renderText();
 }
 
-async function renderText() {}
+const sapmle_tree = [
+  {
+    name: "Customer Onboarding",
+    children: [
+      {
+        name: "Aadhar",
+        children: [
+          {
+            name: "Yes",
+            children: [
+              {
+                name: "Does Customer have PAN ?",
+                children: [
+                  {
+                    name: "Yes",
+                    children: [
+                      {
+                        name: "Has customer filled the Application ?",
+                        children: [
+                          {
+                            name: "Yes",
+                            children: [
+                              {
+                                name: "Done",
+                                children: [],
+                                type: "text",
+                              },
+                            ],
+                            type: "button",
+                          },
+                          {
+                            name: "No",
+                            children: [
+                              {
+                                name: "Request the Customer to do it",
+                                children: [],
+                                type: "text",
+                              },
+                            ],
+                            type: "button",
+                          },
+                        ],
+                        type: "text",
+                      },
+                    ],
+                    type: "button",
+                  },
+                  {
+                    name: "No",
+                    children: [
+                      {
+                        name: "Ask customer to provide PAN",
+                        children: [],
+                        type: "text",
+                      },
+                    ],
+                    type: "button",
+                  },
+                ],
+                type: "text",
+              },
+            ],
+            type: "button",
+          },
+          {
+            name: "No",
+            children: [
+              {
+                name: "Request Aadhar",
+                children: [],
+                type: "text",
+              },
+            ],
+            type: "button",
+          },
+        ],
+        type: "text",
+      },
+    ],
+    type: "root",
+  },
+];
+
+async function renderText() {
+  console.log("Modal");
+  console.log(sapmle_tree);
+  let checklist = ``;
+  console.log(checklist);
+  sapmle_tree?.map((e, i) => {
+    checklist += ` <div class="fw-card-1 fw-p-24 fw-flex fw-flex-row">
+        <section class="fw-flex-grow">
+          <h5 id="selected_" key="${i}" class="fw-type-h5 fw-my-0">${e.name}</h5>
+        </section>
+      </div>`;
+  });
+  $(checklist).appendTo("#checklist_div");
+}
